0d449b653957f358b290b97efee1b69c63212faf
Troy896 <troy896@gmail.com>
2018-09-27 10:59:37 -0500

    no message

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ mkdir Toolkit
 mkdir Toolkit/splashes
 mkdir ReiNX
 mkdir ReiNX/sysmodules
+mkdir ReiNX/sysmodules.dis
 mkdir ReiNX/patches
 mkdir ReiNX/titles
 mkdir ReiNX/titles/010000000000100D
@@ -31,35 +32,16 @@ cp /d/Github/NXCFW/Essentials/boot.dat /d/Github/NXCFW/build/boot.dat
 cp /d/Github/NXCFW/Essentials/splash2.png /d/Github/NXCFW/build/Toolkit/splashes/pokemon.png
 cp /d/Github/NXCFW/Essentials/REICOOL.png /d/Github/NXCFW/build/Toolkit/splashes/Reicool.png
 cp /d/Github/NXCFW/Essentials/Ye.png /d/Github/NXCFW/build/Toolkit/splashes/Yeezy.png
-cp /d/Github/NXCFW/ReiNX/NX_Sysmodules/Makefile /d/Github/NXCFW/ReiNX/NX_Sysmodules/Makefile.bak
-cp /d/Github/NXCFW/ReiNX/Makefile /d/Github/NXCFW/ReiNX/Makefile.bak
-cp /d/Github/NXCFW/ReiNX/NX_Sysmodules/creport/Makefile /d/Github/NXCFW/ReiNX/NX_Sysmodules/creport/Makefile.bak
-cp /d/Github/NXCFW/Essentials/ReiNX.diff /d/Github/NXCFW/ReiNX/patch.diff
-cp /d/Github/NXCFW/Essentials/Modules.diff /d/Github/NXCFW/ReiNX/NX_Sysmodules/patch.diff
-cd /d/Github/NXCFW/ReiNX
-patch -p1 --binary < patch.diff
-cd /d/Github/NXCFW/ReiNX/NX_Sysmodules
-patch -p1 --binary < patch.diff
 cd /d/Github/NXCFW/ReiNX
 make
 cp /d/Github/NXCFW/ReiNX/out/ReiNX.bin /d/Github/NXCFW/build/ReiNX.bin
 cp /d/Github/NXCFW/Essentials/splash.bin /d/Github/NXCFW/build/ReiNX/splash.bin
 cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/loader.kip /d/Github/NXCFW/build/ReiNX/sysmodules/loader.kip
 cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/sm.kip /d/Github/NXCFW/build/ReiNX/sysmodules/sm.kip
-cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/pm.kip /d/Github/NXCFW/build/ReiNX/sysmodules/pm.kip
-cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/fs_mitm.kip /d/Github/NXCFW/build/ReiNX/sysmodules/fs_mitm.kip
-cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/creport.nso /d/Github/NXCFW/build/ReiNX/titles/0100000000000036/exefs/main
-cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules/creport.npdm /d/Github/NXCFW/build/ReiNX/titles/0100000000000036/exefs/main.npdm
+cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules.dis/pm.kip /d/Github/NXCFW/build/ReiNX/sysmodules.dis/pm.kip
+cp /d/Github/NXCFW/ReiNX/out/ReiNX/sysmodules.dis/fs_mitm.kip /d/Github/NXCFW/build/ReiNX/sysmodules/fs_mitm.kip
 cp /d/Github/NXCFW/ReiNX/out/ReiNX/patches/es_patch.rxp /d/Github/NXCFW/build/ReiNX/patches/es_patch.rxp
 make clean
-rm /d/Github/NXCFW/ReiNX/NX_Sysmodules/Makefile
-rm /d/Github/NXCFW/ReiNX/Makefile
-rm /d/Github/NXCFW/ReiNX/NX_Sysmodules/creport/Makefile
-rm /d/Github/NXCFW/ReiNX/patch.diff
-rm /d/Github/NXCFW/ReiNX/NX_Sysmodules/patch.diff
-mv /d/Github/NXCFW/ReiNX/NX_Sysmodules/Makefile.bak /d/Github/NXCFW/ReiNX/NX_Sysmodules/Makefile
-mv /d/Github/NXCFW/ReiNX/Makefile.bak /d/Github/NXCFW/ReiNX/Makefile
-mv /d/Github/NXCFW/ReiNX/NX_Sysmodules/creport/Makefile.bak /d/Github/NXCFW/ReiNX/NX_Sysmodules/creport/Makefile
 cd /d/Github/NXCFW/hbmenu
 make nx
 cp /d/Github/NXCFW/hbmenu/hbmenu.nro /d/Github/NXCFW/build/hbmenu.nro
